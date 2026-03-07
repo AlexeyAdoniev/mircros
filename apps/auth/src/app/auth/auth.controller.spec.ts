b9474fc8264f21efa28610096b11787e79a33d95
AlexAdon <alexadoniev@gmail.com>
2026-03-08 00:24:46 +0200

    fix test

--- apps/auth/src/app/auth/auth.controller.spec.ts
+++ apps/auth/src/app/auth/auth.controller.spec.ts
@@ -1,12 +1,23 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthController } from './auth.controller';
+import { UsersService } from '../users/users.service';
 
 describe('AuthController', () => {
   let controller: AuthController;
 
+  const mockUsersService = {
+    user: jest.fn(),
+  };
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [AuthController],
+      providers: [
+        {
+          provide: UsersService,
+          useValue: mockUsersService,
+        },
+      ],
     }).compile();
 
     controller = module.get<AuthController>(AuthController);
