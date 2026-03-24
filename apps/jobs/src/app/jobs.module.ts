eb1440f794f358cbdfa7fcd8d5dc915912994f0b
AlexAdon <alexadoniev@gmail.com>
2026-03-24 02:06:31 +0200

    change jobs ClientsModule init

--- apps/jobs/src/app/jobs.module.ts
+++ apps/jobs/src/app/jobs.module.ts
@@ -7,19 +7,26 @@ import { ClientsModule, Transport } from '@nestjs/microservices';
 import { AUTH_PACKAGE_NAME } from '@jobber/grpc';
 import { join } from 'path';
 import { PulsarModule } from '@jobber/pulsar';
+import { ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
     DiscoveryModule,
     PulsarModule,
-    ClientsModule.register([
+    ClientsModule.registerAsync([
       {
-        transport: Transport.GRPC,
         name: AUTH_PACKAGE_NAME,
-        options: {
-          package: AUTH_PACKAGE_NAME,
-          protoPath: join(__dirname, '../../libs/grpc/proto/auth.proto'),
+        useFactory: (configService: ConfigService) => {
+          return {
+            transport: Transport.GRPC,
+            options: {
+              url: configService.getOrThrow('AUTH_GRPC_SERVICE_URL'),
+              package: AUTH_PACKAGE_NAME,
+              protoPath: join(__dirname, '../../libs/grpc/proto/auth.proto'),
+            },
+          };
         },
+        inject: [ConfigService],
       },
     ]),
   ],
