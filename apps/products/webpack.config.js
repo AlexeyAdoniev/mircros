6565775b63e6c1814c1aec3612eb1fd67983fccf
AlexAdon <alexadoniev@gmail.com>
2026-04-20 02:10:45 +0300

    change docker configs of executor and products

--- apps/products/webpack.config.js
+++ apps/products/webpack.config.js
@@ -1,25 +1,9 @@
-const { NxAppWebpackPlugin } = require('@nx/webpack/app-plugin');
 const { join } = require('path');
+const { merge } = require('webpack-merge');
+const common = require('../../webpack.apps.config');
 
-module.exports = {
+module.exports = merge(common, {
   output: {
     path: join(__dirname, '../../dist/apps/products'),
-    clean: true,
-    ...(process.env.NODE_ENV !== 'production' && {
-      devtoolModuleFilenameTemplate: '[absolute-resource-path]',
-    }),
   },
-  plugins: [
-    new NxAppWebpackPlugin({
-      target: 'node',
-      compiler: 'tsc',
-      main: './src/main.ts',
-      tsConfig: './tsconfig.app.json',
-      assets: ['./src/assets'],
-      optimization: false,
-      outputHashing: 'none',
-      generatePackageJson: true,
-      sourceMap: true,
-    }),
-  ],
-};
+});
